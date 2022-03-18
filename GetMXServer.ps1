b33e9a9a5fa65c9043634294651ea1d73a92e8bd
Runard <40338703+alexandremottier@users.noreply.github.com>
2022-03-18 13:49:44 +0100

    v1.1
    v1.1

---
+++ GetMXServer.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host -ForegroundColor Yellow "...::: Récupération des enregistrements MX pour un domaine DNS :::..."
+
+while(0 -ne 1)
+{
+    $domain = Read-Host "Entrez le nom du domaine à vérifier"
+    Resolve-DnsName -Name $domain -Type MX | sort Preference -desc | ft Name,NameExchange,Preference
+   # $publicip = (Resolve-DnsName -Name $domain).Address
+   # $ShodanLink = "https://api.shodan.io/shodan/host/" + $publicip + "?key=9r6vVczYqYGR9F3WADASttMPt6fqK2Mm"
+   # $Shodan = Invoke-RestMethod -uri $ShodanLink
+   # $operateur = $Shodan.isp
+   # Write-Host -ForegroundColor Yellow "L'opérateur télécom de l'IP $publicip ($domain) est $operateur ."
+}
